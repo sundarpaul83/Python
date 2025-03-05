8350d0f0051adc578d7c4d999df907be9293fb0e
sundarpaul83 <pkskpaulsamy@gmail.com>
2025-03-05 23:23:33 +0530

    created assign.py bitWis.py

---
+++ assign.py
@@ -0,0 +1,11 @@
+a = 34  # Initialize the value of a
+b = 6  # Initialize the value of b
+
+# Printing the diffferent results
+print("1. a+=b:", a + b)
+print("2. a-=b:", a - b)
+print("3. a*=b:", a * b)
+print("4. a/=b:", a / b)
+print("5. a%=b:", a % b)
+print("6. a**=b:", a**b)
+print("7. a//=b:", a // b)
---
+++ bitWis.py
@@ -0,0 +1,10 @@
+a = 7  # Initializing The Value of a
+b = 8  # Initializing The Value of b
+
+# Printing Different Results
+print("1. Bitwise AND Operator a & b :", a & b)
+print("2. Bitwise OR Operator a | b :", a | b)
+print("3. Bitwise XOR a ^ b :", a ^ b)
+print("4. Bitwise NOT ~a :", ~a)
+print("5. Bitwise Left Shift a << b :", a << b)
+print("6. Bitwise Right Shift a >> b :", a >> b)
